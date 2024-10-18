442b42e31bd0dd486377f43a526b838675378e6f
Rupeshpharande <pharanderupesh@gmail.com>
2024-10-18 10:35:59 +0530

    update changes

---
+++ Patterns/Pattern10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Pattern10
+    {
+        //832 ➞ 594
+        //51 ➞ 36
+        //static void Main(string[] args)
+        //{
+        //    int num = Convert.ToInt32(Console.ReadLine());
+        //    int temp = num;
+        //    int sum= 0;
+        //    while (num > 0) {
+        //        int reverse = num % 10;
+        //        sum = sum*10 + reverse;
+        //        num /= 10;
+        //    }
+        //    int result = temp-sum;
+        //    Console.Write(result);
+        //}
+
+    }
+}
